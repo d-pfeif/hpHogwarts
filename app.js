14e9c35642b027b77c25f93ed8816e33f0390e9e
Dakota J Pfeifer <dakota.pfeifer@yahoo.com>
2017-11-06 13:34:39 -0700

    Adding project to new git account

---
+++ app.js
@@ -0,0 +1,162 @@
+function changeParagraph0() {
+  var el = document.getElementsByClassName('filler')
+  el['0'].innerHTML = "Usual traits of those inducted into Gryffindor house: daring, loyalty, chivalry. The emblematic animal is a lion, and its colours are scarlet and gold. Sir Nicholas de Mimsy-Porpington, also known as Nearly Headless Nick is the house ghost. Gryffindor corresponds roughly to the element of fire, and it is for that reason that the colours scarlet and gold were chosen to represent the House. The colour of fire corresponds to that of a lion as well, with scarlet representing the mane and tail and gold representing the coat."
+}
+
+function changeParagraph1() {
+  var el = document.getElementsByClassName('filler')
+  var img = document.createElement('img')
+  el['0'] = '';
+  img.setAttribute('id', 'godric')
+  img.src = "https://sites.google.com/site/hohthefounders/_/rsrc/1244110553447/godric-gryffindor/Godric%20Gryffindor.gif"
+
+  var par1 =`Godric Gryffindor valued courage, determination, chivalry, and strength of heart, and was described as one of the four most brilliant witches and wizards of his time. He was also tolerant and accepting of Muggle-borns, a point which brought about the end of his friendship with Salazar Slytherin. He was very forward thinking due to his creating the Sorting Hat to preserve the selection of the students after his time. This showcases his cleverness and creativity. He also had a versatile mindset as he chose to wield a sword as well as a wand, making him a rather dynamic man who deferred to the duelling style of his opponent, muggle and wizard alike. Going along with this, it shows that he was respectful and fair, choosing to wield a sword instead of magic against muggle combatants out of a sense of fair play as magic would be a most decidedly unbalanced weapon in a duel against a simple sword.`
+  var par2 = `In addition to showing his sense of fairness, his deference to the duelling style of opponent also shows a need to challenge himself, proving his worth and courage, which became one of the signature traits of his House at Hogwarts. His threat to unsheathe and use his famous blade against Ragnuk the First and his followers shows that despite his noble heart and good intentions, that Godric was very much capable of taking the lives of sapient beings such as Goblins. This would not have been an uncommon trait for a man of his era or personality due to the constant violence that pervaded medieval Europe and the acceptability of duelling to the death among both wizard and muggles alike.`
+  el['0'].innerHTML = par1
+  el['0'].append(img)
+  el['0'].append(par2)
+
+}
+
+function changeParagraph2() {
+  var el = document.getElementsByClassName('filler')
+  var img = document.createElement('img')
+  el['0'].innerHTML = '';
+  img.setAttribute('id', 'godric')
+  img.src = "https://i.pinimg.com/736x/b2/e9/14/b2e914d1a994a39f99287bd95b6c56fb--hp-sauce-the-doors.jpg"
+
+  var par1 = "Minerva McGonagall is deputy Headmistress, head of Gryffindor House, Transfiguration professor, and later Headmistress at Hogwarts, where she began teaching in December 1956. She is described as fair, but very strict."
+  var par2 = "McGonagall is also described as a tall, rather severe-looking woman, with black hair typically drawn into a tight bun. She wears emerald green robes, a pointed hat, and always has a very prim expression. She speaks with a slight Scottish accent. She is a sprightly 70-year-old. McGonagall wears square spectacles that match the markings around the eyes of her Animagus form of a British Shorthair silver tabby cat; she has the distinction of being the first Animagus introduced in the series and being one of the few registered Animagi of the century, though she was rarely seen using the ability. "
+
+  el.innerHTML = par1;
+  el['0'].append(img);
+  el['0'].append(par2);
+}
+
+function changeParagraph3() {
+  var el = document.getElementsByClassName('filler')
+  var img = document.createElement('img')
+  el['0'].innerHTML = '';
+  img.setAttribute('id', 'godric')
+  img.src = "https://vignette2.wikia.nocookie.net/harrypotter/images/5/5e/Albus_Dumbledore_Half-Blood_Prince.JPG/revision/latest?cb=20081115193545"
+
+  var par1 = "A Headmaster or Headmistress is the chief administrator of a School of Magic. They make all major decisions regarding the safety and the day-to-day functioning of the school, and have the power to override any decision made by any other authoritative facilitator at the school, with the possible exception of the Board of Governors and during a brief period in 1996, the High Inquisitor, in Hogwarts. A Headmaster also controls the protective spells surrounding the school and the ability for certain individuals to enter or exit the grounds at will. The four founders of Hogwarts (Helga Hufflepuff, Godric Gryffindor, Salazar Slytherin, and Rowena Ravenclaw ) are not considered heads, despite leading the school during their time."
+  var par2 = "Dumbledore was appointed Headmaster of Hogwarts sometime between March 1965 and March 1971, succeeding Armando Dippet. During this time, he was offered the position of Minister for Magic several times, but he turned down the offer every time, remembering how his quest for power had been his weakness in the past."
+
+  el['0'].append(par2)
+  el['0'].append(img);
+  el['0'].append(par1);
+}
+
+function changeParagraph4(){
+  var el = document.getElementsByClassName('filler')
+  var par1 = 'Students belonging to this house are known to be hard-working, friendly, loyal, honest and rather impartial. It may be that due to their values, Hufflepuffs are not as competitive as the other houses, and are more modest about their accomplishments. Hufflepuff is the most inclusive among the four houses; valuing hard work, dedication, patience, loyalty, and fair play rather than a particular aptitude in its students.Hufflepuff appears to have the least rivalry with the other houses, except in Quidditch, although the Triwizard Tournament did temporarily put a great deal of strain on the relationship between Gryffindor and Hufflepuff, due to unusual circumstances of the selection of both Gryffindor student Harry Potter and Hufflepuff student Cedric Diggory as Champions. This was mended by 1998, where a majority of Hufflepuffs rose to defend Harry from Pansy Parkinson and later helped to defend Hogwarts.'
+  el['0'].innerHTML = ''
+  el['0'].append(par1)
+}
+
+function changeParagraph5() {
+  var el = document.getElementsByClassName('filler')
+  var par1 = `According to Minerva McGonagall, all four of the houses have produced exceptional witches and wizards in their time, but due to Helga Hufflepuff's policy of accepting any student and its usually poor performance in the House Cup, Hufflepuff is often thought to be the house of less talented wizards and witches. However, this is merely a misunderstanding of the Sorting Hat's poem. The Hufflepuff house has produced a great many of successful and influential members of the wizarding world, such as the renowned Magizoologist Newton Scamander. A great number of exceptionally skilled witches and wizards also have their origins on this house, including noted Arithmancer Bridget Wenlock, and former Minister for Magic Artemisia Lufkin.`
+  var par2 = `What makes Hufflepuff differ from the other houses is the fact that it doesn't tend to boast about these accomplishments; it is not considered to be part of their nature. Hufflepuff has produced the fewest number of dark wizards than any other house at Hogwarts. Their cheerful and friendly demeanour can probably account for this property of the House. Hufflepuff firmly demonstrated its loyalty during the Battle of Hogwarts in 1998, when they were second only to Harry Potter's own house of Gryffindor in the number of students willing to stay and fight against Voldemort and his forces in the defence of their school.`
+  var img = document.createElement('img')
+  img.setAttribute('id', 'godric')
+  img.src = "https://vignette.wikia.nocookie.net/harrypotter/images/4/42/Helgaaa.gif/revision/latest?cb=20110727013303"
+
+  el['0'].innerHTML = ''
+  el['0'].append(par1)
+  el['0'].append(img)
+  el['0'].append(par2)
+}
+
+function changeParagraph6() {
+  var el = document.getElementsByClassName('filler')
+  var par1 = "Madam Professor Pomona Sprout (15 May, 1931 or 1941) is a witch who works as Head of Hufflepuff House and Head of the Herbology department at Hogwarts School of Witchcraft and Wizardry. She attended the school in her youth, where she was Sorted into Hufflepuff House and excelled at Herbology. Some time after her graduation, Sprout returned to Hogwarts to teach."
+  var par2 = "Professor Sprout opposed Lord Voldemort during the Second Wizarding War. In 1993, she grew the Mandrakes that would later be used in the Mandrake Restorative Draught to cure those who were petrified by Slytherin's Monster. She also opposed Dolores Umbridge and her tyranny over the school in the 1995-1996 school year, which the rest of the staff were happy to join her in doing, voted for the school to remain open after the Battle of the Astronomy Tower, and assisted in ousting Severus Snape in 1998. Sprout took a group of students to the battlements during the Battle of Hogwarts and threw Devil's Snare and various other dangerous plants onto the invading Death Eaters."
+  var img = document.createElement('img')
+  img.setAttribute('id', 'godric')
+  img.src = "https://vignette1.wikia.nocookie.net/harrypotter/images/7/71/PSprout.png/revision/latest?cb=20110109155242"
+  el['0'].innerHTML = ''
+  el['0'].append(par1)
+  el['0'].append(img)
+  el['0'].append(par2)
+}
+
+function changeParagraph7() {
+  var el = document.getElementsByClassName('filler')
+  var par1 = "Ravenclaw House prizes learning, wisdom, wit, and intellect in its members. Thus, many Ravenclaws tend to be academically motivated and talented students. Ravenclaws also pride themselves in being original in their ideas, and methods. It's not unusual to find Ravenclaw students practising especially different types of magic that other houses might shun. Hermione Granger, an extremely intelligent witch and the top student in her year, was sorted into Gryffindor, though she admitted that the Sorting Hat had seriously considered placing her in Ravenclaw."
+  el['0'].innerHTML = ''
+  el['0'].append(par1)
+}
+
+function changeParagraph8() {
+  var el = document.getElementsByClassName('filler')
+  var par1 = "Ravenclaw is one of the four Houses of Hogwarts School of Witchcraft and Wizardry, founded by Rowena Ravenclaw. Members of this house are characterised by their wit, learning, and wisdom. Its house colours are blue and bronze, and its symbol is an eagle. The house ghost, who in life was the daughter of the house's founder Rowena Ravenclaw, is the Grey Lady. Ravenclaw corresponds roughly to the element of air; the House colours blue and bronze were chosen to represent the sky and eagle feathers respectively, both having much to do with air."
+  var par2 = "Ravenclaw House appears to have a little rivalry with the other houses, except in Quidditch. Harry Potter believed that they tended to side with Gryffindor in its rivalry with Slytherin, as did Hufflepuff house. However, during the 1994 Triwizard Tournament, most Ravenclaws supported Hufflepuff Champion Cedric Diggory over Harry.[9] They believed that Harry had in fact cheated his way into being a champion and that the true Hogwarts champion was Cedric. Many Ravenclaws (such as Cho Chang) joined Dumbledore's Army and fought in the Battle of Hogwarts along with Gryffindors and Hufflepuffs. They rouse at almost at the exact moment as the Hufflepuffs and turned their wands upon Pansy Parkinson and Slytherin House in Harry Potter's defence, when the former wished to hand Harry over to Lord Voldemort and his army of Death Eaters."
+  var img = document.createElement('img')
+  img.setAttribute('id', 'godric')
+  img.src = "https://sites.google.com/site/hohthefounders/_/rsrc/1244110410080/rowena-ravenclaw/Rowena%20Ravenclaw.gif"
+  el['0'].innerHTML = ''
+  el['0'].append(par1)
+  el['0'].append(img)
+  el['0'].append(par2)
+}
+
+function changeParagraph9() {
+  var el = document.getElementsByClassName('filler')
+  var par1 = "Professor Filius Flitwick is a part-goblin, part-wizard who attended Hogwarts School of Witchcraft and Wizardry and was sorted into Ravenclaw House, being an intelligent young man and a model student. Some time after his graduation, Flitwick returned to Hogwarts and became the Charms Master, as well as the Head of Ravenclaw House."
+  var par2 = "During the First and Second Wizarding Wars, Flitwick opposed Lord Voldemort. In 1995, he also opposed Dolores Umbridge, the High Inquisitor of Hogwarts, and, in 1997, fought at the Battle of the Astronomy Tower, after which he attended the Funeral of Albus Dumbledore. He also protected the students from the Carrows in the 1997-1998 school year. On 2 May 1998, he partook in the Battle of Hogwarts and defeated many Death Eaters. He survived the war, and likely continued to teach Charms at Hogwarts to future generations of students."
+  var img = document.createElement('img')
+  img.setAttribute('id', 'godric')
+  img.src = "https://vignette.wikia.nocookie.net/harrypotter/images/c/c6/20100111183429%21Flitwick_HBPoster_Lighter.png/revision/latest/scale-to-width-down/120?cb=20100310181520"
+  el['0'].innerHTML = ''
+  el['0'].append(par1)
+  el['0'].append(img)
+  el['0'].append(par2)
+}
+
+function changeParagraph10() {
+  var el = document.getElementsByClassName('filler')
+  var par1 = "Slytherin is one of the four Houses at Hogwarts School of Witchcraft and Wizardry, founded by Salazar Slytherin. Salazar instructed the sorting hat to choose a few particular characteristics he most valued. Such character traits are cunning, resourcefulness, and ambition (mostly of pure-blood pure-blood supremacy, due to his mistrust of Muggle-borns). Its emblematic animal is a snake and its colours are green and silver. The Head of Slytherin is Horace Slughorn (who left the role to Severus Snape for fifteen years before taking it up again in 1996) and the House's patron ghost is the Bloody Baron. Slytherin corresponds roughly with the element of water with serpents being commonly associated with the sea and lochs in western European mythology, as well as serpents being physically fluid and flexible animals. The colours also correspond with waters around lakes and lochs often being green, and silver being often associated with grey rain water."
+  el['0'].innerHTML = ""
+  el['0'].append(par1)
+}
+
+function changeParagraph11() {
+  var el = document.getElementsByClassName('filler')
+  var par1 = "Contrary to the wishes of the other founders of Hogwarts School of Witchcraft and Wizardry, Salazar Slytherin wanted a stricter policy of admission to the school —one limited strictly to pure-blood students. The controversy this created apparently led to him leaving the school and the Chamber of Secrets behind. A millennium later, the house's students are still considered affiliated with Salazar's reputed views, and open expression of pro-blood purity sentiment does seem to be more common in Slytherin than in other Houses. Also, no one can really blame him for not letting Muggleborns into Hogwarts, when Muggles were actively trying to kill all wizards, regardless of intent at the time."
+  var par2 = "This disagreement over blood purity, which caused Salazar Slytherin's friendship with Godric Gryffindor to break, is the origin of the feud between Slytherin and Gryffindor, the two most competitive of the houses. Slytherin and Gryffindor are generally rivals in Quidditch and for the House Cup, which the former had won seven years in a row prior to Harry Potter's arrival at Hogwarts in 1991. It is also rare for Slytherin and Gryffindor students to be friendly with one another, and common for them to share animosity. This was particularly the case during the First and Second Wizarding Wars. "
+  var img = document.createElement('img')
+  img.setAttribute('id', 'godric')
+  img.src = "https://sites.google.com/site/hohthefounders/_/rsrc/1244110513106/salazar-slytherin/Salazar%20Slytherin.gif"
+  el['0'].innerHTML = ''
+  el['0'].append(par1)
+  el['0'].append(img)
+  el['0'].append(par2)
+}
+
+function changeParagraph12() {
+  var el = document.getElementsByClassName('filler')
+  var par1 = "Professor Horace Eugene Flaccus Slughorn was a pure-blood or half-blood wizard. He attended Hogwarts School of Witchcraft and Wizardry as a member of Slytherin before returning in 1931 as Potions Master. He also served as Head of Slytherin House, before retiring in 1981 due to a fear that Albus Dumbledore would discover that he had told Tom Riddle about Horcruxes."
+  var par2 = "Following the return of Voldemort and the Death Eaters beginning to seek Slughorn out, the retired professor lived a life on the run, unsure if Voldemort intended to recruit him or kill him to keep his Horcruxes a secret. In 1996, Dumbledore convinced Slughorn to return to Hogwarts by tempting him out of retirement with the prospect of collecting Harry Potter as he had so many other students, with his true goal being to learn what Slughorn had told Riddle. Slughorn eventually divulged his true memory. He continued to teach during the 1997–1998 school year under Voldemort's regime, and was surprised to learn that Voldemort meant to recruit him only to teach the young wizards that Voldemort would deem suitable to attend Hogwarts."
+  var img = document.createElement('img')
+  img.setAttribute('id', 'godric')
+  img.src = "https://vignette.wikia.nocookie.net/harrypotter/images/e/e0/Horace_Slughorn.jpg/revision/latest/scale-to-width-down/319?cb=20121226172917"
+  el['0'].innerHTML = ''
+  el['0'].append(par1)
+  el['0'].append(img)
+  el['0'].append(par2)
+}
+
+function invitation(){
+  var userPrompt = prompt('We will test to see if you posses magic abilities...please enter your name and wait for a response...', 'Harry Potter');
+  if (userPrompt == '' || userPrompt == null) {
+    alert('Invalid Input')
+
+  } else {
+    setTimeout(function() {
+      alert('CONGRATULATIONS! You\'ve been accepted into Hogwarts! Expect a letter within the next few days.')
+    }, 3000)
+
+    }
+  }
---
+++ news.js
@@ -0,0 +1,22 @@
+fetch('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=harrypotter&sort=newest&api-key=606e61abf0b846909e1db653220e47be')
+.then((res)=>{
+  return res.json()
+  .then ((newsData)=>{
+    console.log(newsData.response.docs)
+    var serial = document.getElementById('serial');
+    console.log(serial)
+    for(var i = 0; i < 5; i++){
+      var aTag = document.createElement('a')
+      var div = document.createElement('div')
+      var pTag = document.createElement('p')
+
+      aTag.href = newsData.response.docs[i].web_url;
+      div.className = "media";
+      div.innerText = newsData.response.docs[i].headline.main;
+
+      aTag.append(div);
+      // div.append(pTag);
+      serial.append(aTag);
+    }
+  })
+})
